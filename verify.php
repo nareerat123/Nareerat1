d0cb92f102ee69194da35b8ce68d0de7321a21f7
nareerat123 <36217377+nareerat123@users.noreply.github.com>
2018-02-07 11:27:23 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
-$access_token = 'njUoOMne4J+S1pHSJ3z7DMaA6HrTXc5oQb4gL3gSHqLSK9sMNAk35Mi+a76Rmosrh+tHEWD9N5yy9EU4RVLuf2fGUJFarIgK2dWAdNr4tmxmnBC/ODdTVWh1e2x2+PgrjKZt9R2ZeN7aUMax1639LgdB04t89/1O/w1cDnyilFU=
-;
+$access_token = '3VisO7kh0YLge2w2eAX2T0kybXF7EMK7suWyewx4IMm8JXKi/PYTEuoI+tIf6bkWh+tHEWD9N5yy9EU4RVLuf2fGUJFarIgK2dWAdNr4tmz4CuHJiwl4agEJUTL8yNnXFYAMIwFGP4aO3/Ij73USogdB04t89/1O/w1cDnyilFU=';
+
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
